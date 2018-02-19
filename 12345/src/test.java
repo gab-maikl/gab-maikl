ca86a95130a0f2559c2a154c41add780e68d8558
1 <1@100.98.168.185>
2018-02-19 20:33:34 +0300

    test

--- 12345/src/test.java
+++ 12345/src/test.java
@@ -5,11 +5,16 @@ import org.junit.Test;
 public class test {
 
 	@org.junit.Test
-	public void testSharafutdinov() {
+	public void test() {
 		gui a = new gui();
 		a.start();
-		a.t2.setValue(2);
+		a.t15.setValue(225.25);
+		a.t16.setValue(45.14);
+		a.t8.setValue(121.92);
+		a.t7.setValue(2);
+		a.t3.setValue(800);
+		a.t1.setValue(31);
 		a.Confirm.doClick();
-		assertEquals(a.res9.getText(), "1373,99");
+		assertEquals(a.res4.getText(), "183,94");
 	}
 }
